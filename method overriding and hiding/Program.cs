83565d665a2782ac7cd666fe86b212717ce4df84
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-09-01 12:29:31 +0530

    Add project files.

---
+++ method overriding and hiding/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace method_overriding_and_hiding
+{
+
+    class BaseClass
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Base class");
+        }
+    }
+
+    class DerivedClass : BaseClass
+    {
+        //method hiding
+        /* public new void Print()
+         {
+             Console.WriteLine("Derived Class");
+         }*/
+
+        //method overriding
+        public override void Print()
+        {
+            Console.WriteLine("Derived Class");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BaseClass B = new DerivedClass();
+            B.Print();
+            Console.ReadLine();
+        }
+
+    }
+}
